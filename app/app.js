dd5cdd9a0c262abee2d4cdbf2e0ffeb143163aad
jooa <dlwndk4677@gmail.com>
2022-08-17 22:06:45 +0900

    refactore the folders

---
+++ app/app.js
@@ -0,0 +1,21 @@
+//module
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+//routing 
+const home = require("./src/routes/home");
+
+//app setting
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+
+
+
+//현재폴더 -> routes -> home 안에 있는 js 를 불러와줘 
+
+app.use("/", home); // use -> middleware 를 등록해주는 메소드 
+
+
+//3000번포트 - 내보내주기 
+module.exports = app;
