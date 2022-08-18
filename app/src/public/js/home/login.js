502f32e29e10333645082b53732ca229213c8e9e
jooa <dlwndk4677@gmail.com>
2022-08-18 23:37:12 +0900

    use nodemon

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,2 +1,11 @@
 "use strict";
-console.log("hello");
+// console.log("hello");
+// console.log("bye");
+
+const id = document.querySelector("#id"),
+    psword = document.querySelector("#psword"),
+    loginBtn = document.querySelector("button");
+
+    console.log(id);
+    console.log("hello")
+
