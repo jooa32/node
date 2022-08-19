ea9052cdcaee01ec8fb5356e5c7169cdca3bf8a5
jooa <dlwndk4677@gmail.com>
2022-08-19 12:27:48 +0900

    saved html object by using DOM

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,11 +1,24 @@
 "use strict";
+
+
+
+const id = document.querySelector("#id");
+const psword = document.querySelector("#psword");
+const loginBtn = document.querySelector("button");
+
+function login(){
+    // console.log("bye");
+    const req = {
+        id: id.value,
+        psword: psword.value,
+    };
+    console.log(req);
+}
+
+loginBtn.addEventListener("click", login);
+
+
+// console.log(id);
 // console.log("hello");
-// console.log("bye");
 
-const id = document.querySelector("#id"),
-    psword = document.querySelector("#psword"),
-    loginBtn = document.querySelector("button");
-
-    console.log(id);
-    console.log("hello")
-
+    
